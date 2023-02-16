857325d705cab3f9e44736bbd717c734eada42db
ismailnurudeen <ibrightstar247@gmail.com>
2023-02-16 16:31:25 +0100

    feat: created an custom AppBar with slot API to accept a searchbar.

---
+++ app/src/main/java/com/walletconnect/codingchallenge/ui/clothinglist/parts/AppBar.kt
@@ -0,0 +1,39 @@
+package com.walletconnect.codingchallenge.ui.clothinglist.parts
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun AppBar(title: String, modifier: Modifier = Modifier, searchBar: @Composable () -> Unit = {}) {
+    Box(
+        modifier = modifier
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(bottomStartPercent = 30, bottomEndPercent = 30)
+            )
+            .padding(start = 16.dp, end = 16.dp, top = 32.dp, bottom = 16.dp)
+    ) {
+        Column {
+            Text(
+                text = title,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Black,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+            searchBar()
+        }
+    }
+}
